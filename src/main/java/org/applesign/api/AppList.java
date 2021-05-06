8b64acdada1eca1fa09234c2b59d7f7b35e81a7c
apple-sign <noreply@applesign.org>
2021-05-06 15:42:29 +0700

    增加命令行参数

--- src/main/java/org/applesign/api/AppList.java
+++ src/main/java/org/applesign/api/AppList.java
@@ -5,17 +5,18 @@ import org.applesign.utils.MD5Sign;
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
+import org.apache.commons.cli.*;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class AppList {
-    private static final String account = "test"; // 用户名
-    private static final String passwd = "980980"; // 用户登录密码 作为加密的盐值
+
     private static final String API_APPLIST = "https://applesign.org/api/third/appList";
 
-    private void list()
+    private void list(String account, String passwd)
     {
         long timestamp = System.currentTimeMillis() / 1000;
 
@@ -31,21 +32,47 @@ public class AppList {
         params.put("secret",newSign);
 
         try {
-            System.out.println(params.toString());
+            System.out.println(params);
             HashMap<String, String> headers = new HashMap<String, String>();
             headers.put("Content-Type","application/json;charset=utf-8");
             HttpResponse httpResponse = HttpUtils.doPost(API_APPLIST, null, headers, params, new HashMap<>());
             String respStr = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
             System.out.println(respStr);
             Header[] responseAllHeaders = httpResponse.getAllHeaders();
-            System.out.println(responseAllHeaders);
+            System.out.println(Arrays.toString(responseAllHeaders));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
     public static void main(String[] args){
+        String account = "test"; // 用户名
+        String passwd = "980980"; // 用户登录密码 作为加密的盐值
+        Options options = new Options();
+        Option userO = new Option("u", "user", true, "Username");
+        userO.setRequired(true);
+        options.addOption(userO);
+
+        Option passwdO = new Option("p", "passwd", true, "Password");
+        passwdO.setRequired(true);
+        options.addOption(passwdO);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+        System.out.println(Arrays.toString(args));
+        if (args.length > 2) {
+            try {
+                cmd = parser.parse(options, args);
+                account = cmd.getOptionValue("user");
+                passwd = cmd.getOptionValue("passwd");
+            } catch (ParseException e) {
+                System.out.println(e.getMessage());
+                formatter.printHelp("utility-name", options);
+                System.exit(1);
+            }
+        }
         AppList tl = new AppList();
-        tl.list();
+        tl.list(account, passwd);
     }
 }
